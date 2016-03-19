91c69a5481a18a7b2f02a31ab05140bcd03eff2e
Flakebi <flakebi@t-online.de>
2016-03-19 18:40:21 +0100

    Moore text

--- 1-TRAE/error.py
+++ 1-TRAE/error.py
@@ -13,21 +13,22 @@ def readFile(filename):
             res = res + [(float(ls[0]), float(ls[1]))]
     return res
 
-def linearErrors(xs, ys, a_0, a_1):
-    o_y = sqrt(sum([(y - (a_0 + a_1 * x))**2 for (x, y) in zip(xs, ys)]) / (len(xs) - 2))
+def linearErrors(xs, ys, a_0, a_1, o_y = 0):
+    o_y = max(sqrt(sum([(y - (a_0 + a_1 * x))**2 for (x, y) in zip(xs, ys)]) / (len(xs) - 2)), o_y)
     xs2 = [x * x for x in xs]
     d = len(xs) * sum(xs2) - sum(xs) ** 2
     da_0 = o_y * sqrt(sum(xs2) * 1.0 / d)
     da_1 = o_y * sqrt(len(xs) * 1.0 / d)
     return da_0, da_1
 
-xs = [-180, -135, -90, -45, 45, 90, 135, 180]
-ys = [-0.09, -0.19, -0.29, -0.39, 0.1, 0.2, 0.3, 0.39]
-#o_y = 0.03
+#xs = [-180, -135, -90, -45, 45, 90, 135, 180]
+#ys = [-0.09, -0.19, -0.29, -0.39, 0.1, 0.2, 0.3, 0.39]
+xs, ys = zip(*sorted(readFile("A1.txt")))
+o_y = 0.03
 a_0 = 0.00375
 a_1 = 0.00217
-_, da_1 = linearErrors(xs, ys, a_0, a_1)
-#print("A1:", da_1)
+da_0, da_1 = linearErrors(xs, ys, a_0, a_1, o_y)
+print("A1:", da_0, ";", da_1)
 
 xs = [0, 0.08, 0.11, 0.15, 0.19] * 2
 ys = [0.714, 1.23, 1.505, 2.05, 2.54] + [0.717, 1.24, 1.569, 2.06, 2.55]
